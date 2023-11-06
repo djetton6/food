17f706adfa4e20366c8f430f8e792d71fbd2d5d0
Desmend Jetton <desmendjetton@gmail.com>
2023-11-06 00:18:08 -0600

    adding new searchbar style property

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import {View, Text, Screen} from 'react-native';
+
+
+const SearchBar = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default SearchBar;
